6fb72684cf6dbce8370346a1b80ae27a525f1433
diegogb1999 <diegogb1999@gmail.com>
2024-04-25 12:10:45 +0200

    poder seleccionar los nodos

--- src/components/Modelo3D.jsx
+++ src/components/Modelo3D.jsx
@@ -7,6 +7,24 @@ import gsap from "gsap";
 import { Debug } from "@react-three/cannon";
 //import CCapture from "ccapture.js";
 
+function NodeSelection({ nodeNames, onSelectNode, selectedNode }) {
+  return (
+    <div>
+      {nodeNames.map(name => (
+        <button key={name} onClick={() => onSelectNode(name)} style={{ display: 'block', margin: '5px' }}>
+          {name}
+        </button>
+      ))}
+      {selectedNode && <div className="selected">
+        <span>---------------</span>
+        <br></br>
+        {selectedNode.name}
+        </div>}
+    </div>
+  );
+}
+
+
 const Modelado3D = () => {
 
   const [sliderValue, setSliderValue] = useState(50);
@@ -15,35 +33,38 @@ const Modelado3D = () => {
   const mediaRecorderRef = useRef(null);
   const chunks = [];
 
+  const [nodeNames, setNodeNames] = useState([]);
+  const [selectedNode, setSelectedNode] = useState(null);
+
   const [keyframes, setKeyframes] = useState({
     start: { positionZ: null, rotationZ: null },
     end: { positionZ: null, rotationZ: null }
   });
   const [playAnimation, setPlayAnimation] = useState(false);
 
-// Manejadores para los botones
-const handleSaveStart = () => {
-  if (sceneComponentRef.current && sceneComponentRef.current.saveKeyframe) {
-    sceneComponentRef.current.saveKeyframe('start', sliderValue);
-    console.log('Save Start:', sliderValue); // Verificar que el botón está funcionando
-  }
-};
+  // Manejadores para los botones
+  const handleSaveStart = () => {
+    if (sceneComponentRef.current && sceneComponentRef.current.saveKeyframe) {
+      sceneComponentRef.current.saveKeyframe('start', sliderValue);
+      console.log('Save Start:', sliderValue); // Verificar que el botón está funcionando
+    }
+  };
 
-const handleSaveEnd = () => {
-  if (sceneComponentRef.current && sceneComponentRef.current.saveKeyframe) {
-    sceneComponentRef.current.saveKeyframe('end', sliderValue);
-    console.log('Save End:', sliderValue); // Verificar que el botón está funcionando
-  }
-};
+  const handleSaveEnd = () => {
+    if (sceneComponentRef.current && sceneComponentRef.current.saveKeyframe) {
+      sceneComponentRef.current.saveKeyframe('end', sliderValue);
+      console.log('Save End:', sliderValue); // Verificar que el botón está funcionando
+    }
+  };
 
-const handlePlayAnimation = () => {
-  console.log('Play Animation:', keyframes); // Verificar los keyframes antes de animar
-  setPlayAnimation(true);
-  if (sceneComponentRef.current && sceneComponentRef.current.animateKeyframes) {
-    sceneComponentRef.current.animateKeyframes();
-  }
-  setTimeout(() => setPlayAnimation(false), 2000);
-};
+  const handlePlayAnimation = () => {
+    console.log('Play Animation:', keyframes); // Verificar los keyframes antes de animar
+    setPlayAnimation(true);
+    if (sceneComponentRef.current && sceneComponentRef.current.animateKeyframes) {
+      sceneComponentRef.current.animateKeyframes();
+    }
+    setTimeout(() => setPlayAnimation(false), 2000);
+  };
 
   const handleSliderChange = (event) => {
     setSliderValue(Number(event.target.value));
@@ -57,7 +78,21 @@ const handlePlayAnimation = () => {
     mediaRecorderRef.current.stop();
   };
 
+  const onSelectNode = (nodeName) => {
+    sceneComponentRef.current.handleSelectNode(nodeName);
+    setTimeout(() => {
+      setSelectedNode(sceneComponentRef.current.getSelectedNode());
+    }, 0);
+  };
+
   useEffect(() => {
+    if (sceneComponentRef.current) {
+      setNodeNames(sceneComponentRef.current.getNodesNames());
+      // Asigna un nodo seleccionado inicial si es necesario
+      setSelectedNode(sceneComponentRef.current.getSelectedNode());
+    }
+
+
     if (canvasRef.current) {
       const stream = canvasRef.current.captureStream(25);
       mediaRecorderRef.current = new MediaRecorder(stream, { mimeType: 'video/webm' });
@@ -85,26 +120,44 @@ const handlePlayAnimation = () => {
     return () => {
       mediaRecorderRef.current = null;
     };
-  }, []);
+  }, [sceneComponentRef.current]);
 
   return (
-    <div>
-      <Canvas ref={canvasRef} style={{ width: '100vw', height: '700px' }} className="cursor-pointer" frameloop="always" shadows camera={{ position: [-4, 3, 6], fov: 75, near: 0.1, far: 200 }}>
-        <SceneComponent ref={sceneComponentRef} setSliderValue={setSliderValue} sliderValue={sliderValue} keyframes={keyframes}
-  setKeyframes={setKeyframes} // Asegúrate de que esto está siendo pasado correctamente
-  playAnimation={playAnimation} />
-      </Canvas>
-      <div style={{ position: 'absolute', top: '10px', left: '800px' }}>
-      <button className= "boton" onClick={handleSaveStart}>Guardar Keyframe Inicial</button>
-      <button className= "boton" onClick={handleSaveEnd}>Guardar Keyframe Final</button>
-      <button className= "boton" onClick={handlePlayAnimation}>Reproducir Animación</button>
-        <button className = "boton" onClick={startRecording}>Start Recording</button>
-      <button className = "boton" onClick={stopRecording}>Stop Recording</button>
+    <div className="pantalla">
+
+      <div style={{ position: 'absolute', top: '0px', left: '0px' }}>
+
+        <button className="boton" onClick={handleSaveStart}>Guardar Keyframe Inicial</button>
+        <button className="boton" onClick={handleSaveEnd}>Guardar Keyframe Final</button>
+        <button className="boton" onClick={handlePlayAnimation}>Reproducir Animación</button>
+        <button className="boton" onClick={startRecording}>Start Recording</button>
+        <button className="boton" onClick={stopRecording}>Stop Recording</button>
+
       </div>
-      <div style={{ position: 'absolute', top: '10px', left: '10px' }}>
+
+      <div className="nodosCanvas">
+
+        <div className="nodos">
+          <NodeSelection nodeNames={nodeNames} onSelectNode={onSelectNode} selectedNode={selectedNode} />
+        </div>
+
+        <div className="canvas">
+          <Canvas ref={canvasRef} style={{ width: '1500px', height: '700px', background: "black" }} className="cursor-pointer" frameloop="always" shadows camera={{ position: [-4, 3, 6], fov: 75, near: 0.1, far: 200 }}>
+            <SceneComponent ref={sceneComponentRef} setSliderValue={setSliderValue} sliderValue={sliderValue} keyframes={keyframes}
+              setKeyframes={setKeyframes}
+              playAnimation={playAnimation} />
+          </Canvas>
+        </div>
+
+      </div>
+
+      <div className="slider">
+
         <input type="range" min="22" max="250" value={sliderValue} onChange={handleSliderChange} style={{ width: '300px', height: '25px' }} />
         <span>Valor del Slider: {sliderValue}</span>
+
       </div>
+
     </div>
   );
 };
@@ -119,6 +172,18 @@ const SceneComponent = forwardRef(({ setSliderValue, sliderValue, keyframes, set
 
   const nutBox = new Box3().setFromObject(nut);
   const screwBox = new Box3().setFromObject(screw);
+
+  const [selectedNode, setSelectedNode] = useState(null);
+  const handleSelectNode = nodeName => {
+    const node = nodes[nodeName];
+    if (selectedNode) {
+      // Restaurar material previo si es necesario
+    }
+    // Actualizar el material para resaltar el nodo seleccionado
+    setSelectedNode(node);
+    // Más lógica si necesitas actualizar algo más al seleccionar
+  };
+
 
   const saveKeyframe = (type) => {
     const newState = {
@@ -145,17 +210,32 @@ const SceneComponent = forwardRef(({ setSliderValue, sliderValue, keyframes, set
   };
 
   useImperativeHandle(ref, () => ({
+
+    getNodesNames: () => Object.keys(nodes), // Función para obtener los nombres de los nodos
+
+    handleSelectNode: nodeName => { // Función para manejar la selección de un nodo
+      const node = nodes[nodeName];
+      if (selectedNode) {
+        // Aquí puedes restablecer el estado visual anterior del nodo si es necesario
+      }
+      // Actualiza el material para resaltar el nodo seleccionado o cualquier otra lógica necesaria
+      setSelectedNode(node);
+    },
+
+    getSelectedNode: () => selectedNode,
+
     saveKeyframe: (type, value) => {
       const scale = value / 250;
       const positionZ = scale * 3 - 1; // Esto calcula la nueva posición Z basada en el slider
       const rotationZ = scale * Math.PI * 3; // Esto calcula la nueva rotación Z basada en el slider
-      
+
 
       const newState = {
         positionZ: positionZ,
-      rotationZ: rotationZ
+        rotationZ: rotationZ
       };
-  
+
+
       setKeyframes(prevState => ({
         ...prevState,
         [type]: newState
@@ -164,17 +244,17 @@ const SceneComponent = forwardRef(({ setSliderValue, sliderValue, keyframes, set
     animateKeyframes: () => {
       if (keyframes.start && keyframes.end) {
         console.log('Animating from:', keyframes.start, 'to', keyframes.end);
-        
+
         gsap.fromTo(nut.position,
-          { z: keyframes.start.positionZ }, 
+          { z: keyframes.start.positionZ },
           {
             z: keyframes.end.positionZ,
             duration: 2,
             ease: "linear"
           }
         );
-        gsap.fromTo(nut.rotation, 
-          { z: keyframes.start.rotationZ }, 
+        gsap.fromTo(nut.rotation,
+          { z: keyframes.start.rotationZ },
           {
             z: keyframes.end.rotationZ,
             duration: 2,
